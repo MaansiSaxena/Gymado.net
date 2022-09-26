c272eb5ed5e72cfd1f1c0429c615d1b96d89802e
Mansi Saxena <Mansi.Saxena@bhavnacorp.com>
2022-09-26 22:38:31 +0530

    first commit

---
+++ WebApplication3/Controllers/EcommerceController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Controllers
+{
+    public class EcommerceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
